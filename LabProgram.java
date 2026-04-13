252e9e584e0eb15094873f903148114e502a4114
Nickolas450 <nickolas.al340@gmail.com>
2026-04-13 15:10:45 -0600

    Update LabProgram.java

--- LabProgram.java
+++ LabProgram.java
@@ -3,9 +3,27 @@ import java.util.Scanner;
 public class LabProgram {
    
    public static int fibonacci(int n) {
-      /* Type your code here. */
+      if (n < 0) {
+         return -1;
    }
-   
+
+   if (n == 0) return 0;
+   if (n == 1) return 1;
+
+   int prev1 = 0;
+   int prev2 = 1;
+   int current = 0;
+
+   for (int i = 2; i <= n; i++) {
+      current = prev1 + prev2;
+      prev1 = prev2;
+      prev2 = current;
+   }
+
+   return prev2;
+}
+
+      
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
       int startNum;
